370726159dab73fb41bdcc1f2dd1fbda605f725e
Utat Uav <uav@utat.skule.ca>
2015-01-01 17:30:16 -0500

    Probably outdated...
    yup

--- build-groundstation-Desktop_Qt_5_3_MSVC2012_OpenGL_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-groundstation-Desktop_Qt_5_3_MSVC2012_OpenGL_32bit-Debug/debug/moc_mainwindow.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'mainpicwidget.h'
+** Meta object code from reading C++ file 'mainwindow.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../groundstation/mainpic/mainpicwidget.h"
+#include "../../groundstation/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'mainpicwidget.h' doesn't include <QObject>."
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.3.2. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -18,26 +18,29 @@
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_MainPicWidget_t {
-    QByteArrayData data[3];
-    char stringdata[26];
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[5];
+    char stringdata[44];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_MainPicWidget_t, stringdata) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_MainPicWidget_t qt_meta_stringdata_MainPicWidget = {
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
-QT_MOC_LITERAL(0, 0, 13),
-QT_MOC_LITERAL(1, 14, 10),
-QT_MOC_LITERAL(2, 25, 0)
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 11),
+QT_MOC_LITERAL(2, 23, 0),
+QT_MOC_LITERAL(3, 24, 13),
+QT_MOC_LITERAL(4, 38, 5)
     },
-    "MainPicWidget\0toggleMode\0"
+    "MainWindow\0resizeEvent\0\0QResizeEvent*\0"
+    "event"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_MainPicWidget[] = {
+static const uint qt_meta_data_MainWindow[] = {
 
  // content:
        7,       // revision
@@ -51,48 +54,47 @@ static const uint qt_meta_data_MainPicWidget[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x0a /* Public */,
+       1,    1,   19,    2, 0x0a /* Public */,
 
  // slots: parameters
-    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 3,    4,
 
        0        // eod
 };
 
-void MainPicWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        MainPicWidget *_t = static_cast<MainPicWidget *>(_o);
+        MainWindow *_t = static_cast<MainWindow *>(_o);
         switch (_id) {
-        case 0: _t->toggleMode(); break;
+        case 0: _t->resizeEvent((*reinterpret_cast< QResizeEvent*(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
-const QMetaObject MainPicWidget::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_MainPicWidget.data,
-      qt_meta_data_MainPicWidget,  qt_static_metacall, 0, 0}
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
 };
 
 
-const QMetaObject *MainPicWidget::metaObject() const
+const QMetaObject *MainWindow::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *MainPicWidget::qt_metacast(const char *_clname)
+void *MainWindow::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_MainPicWidget.stringdata))
-        return static_cast<void*>(const_cast< MainPicWidget*>(this));
-    return QWidget::qt_metacast(_clname);
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
 }
 
-int MainPicWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
-    _id = QWidget::qt_metacall(_c, _id, _a);
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
